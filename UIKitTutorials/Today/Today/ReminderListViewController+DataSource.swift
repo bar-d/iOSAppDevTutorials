161298ec117ceb30764c361bf8be0694760c9aa2
bard <schy2503@naver.com>
2022-06-11 19:02:54 +0900

    refactor: 프로토콜을 따른 식별자로 변경

--- UIKitTutorials/Today/Today/ReminderListViewController+DataSource.swift
+++ UIKitTutorials/Today/Today/ReminderListViewController+DataSource.swift
@@ -11,8 +11,8 @@ extension ReminderListViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
     
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
-        let reminder = Reminder.sampleData[indexPath.item]
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
+        let reminder = reminders[indexPath.item]
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
