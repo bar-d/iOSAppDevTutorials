8b03b7536c25a2d1e5dab7210175f291256d0aa3
bard <schy2503@naver.com>
2022-06-11 22:03:59 +0900

    feat: ReminderViewController, ReminderViewController+Row 파일 생성 및 구현

---
+++ UIKitTutorials/Today/Today/ReminderViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by 김동용 on 2022/06/11.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .viewDate:
+            return reminder.dueDate.dayText
+        case .viewNotes:
+            return reminder.notes
+        case .viewTime:
+            return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .viewTitle:
+            return reminder.title
+        }
+    }
+}
