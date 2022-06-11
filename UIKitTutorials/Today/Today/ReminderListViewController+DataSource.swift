ef373705bad7f7ebd203d697fcf6afe42858921f
bard <schy2503@naver.com>
2022-06-11 18:17:22 +0900

    feat: ReminderListViewController extension 생성

--- UIKitTutorials/Today/Today/ReminderListViewController+DataSource.swift
+++ UIKitTutorials/Today/Today/ReminderListViewController+DataSource.swift
@@ -5,4 +5,17 @@
 //  Created by 김동용 on 2022/06/11.
 //
 
-import Foundation
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        cell.contentConfiguration = contentConfiguration
+    }
+}
