3c1c58152f87dc4cc82719336ca1d313a5507fdb
bard <schy2503@naver.com>
2022-06-12 19:34:22 +0900

    feat: ReminderViewController extension 구현

--- UIKitTutorials/Today/Today/ReminderViewController+CellConfiguration.swift
+++ UIKitTutorials/Today/Today/ReminderViewController+CellConfiguration.swift
@@ -5,4 +5,14 @@
 //  Created by 김동용 on 2022/06/12.
 //
 
-import Foundation
+import UIKit
+
+extension ReminderViewController {
+    func defaultConfiguration(for cell: UICollectionViewListCell, at row: Row) -> UIListContentConfiguration {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        return contentConfiguration
+    }
+}
