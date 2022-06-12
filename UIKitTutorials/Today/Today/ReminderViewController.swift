22febfc329617322e189c79c7ec5a7cfd528c55f
bard <schy2503@naver.com>
2022-06-12 19:39:15 +0900

    refactor: ReminderViewController내 text함수 extension으로 추출

--- UIKitTutorials/Today/Today/ReminderViewController.swift
+++ UIKitTutorials/Today/Today/ReminderViewController.swift
@@ -88,19 +88,4 @@ class ReminderViewController: UICollectionViewController {
         }
         return section
     }
-    
-    func text(for row: Row) -> String? {
-        switch row {
-        case .viewDate:
-            return reminder.dueDate.dayText
-        case .viewNotes:
-            return reminder.notes
-        case .viewTime:
-            return reminder.dueDate.formatted(date: .omitted, time: .shortened)
-        case .viewTitle:
-            return reminder.title
-        default:
-            return nil
-        }
-    }
 }
